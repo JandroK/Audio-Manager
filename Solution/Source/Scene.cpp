cdfa1602952d5f34082973cbee85f8b2999a2737
JandroK <61042882+JandroK@users.noreply.github.com>
2021-04-06 17:08:36 +0200

    Handout/Solution

---
+++ Solution/Source/Scene.cpp
@@ -0,0 +1,259 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#define MARGIN 50
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	// Load Textures and Fx
+	bg = app->tex->Load("Assets/Textures/speaker.png");
+	laserR = app->tex->Load("Assets/Textures/bullet_red.png");
+	laserB = app->tex->Load("Assets/Textures/bullet_blue.png");
+	laserFx = app->audio->LoadFx("Assets/Audio/Fx/fx_laser.wav");
+
+	// Get dimension of textures
+	SDL_QueryTexture(bg, NULL, NULL, &dimensionBg.x, &dimensionBg.y);
+	SDL_QueryTexture(laserR, NULL, NULL, &dimensionLaserR.x, &dimensionLaserR.y);
+	SDL_QueryTexture(laserB, NULL, NULL, &dimensionLaserB.x, &dimensionLaserB.y);
+
+	player.x = (WINDOW_WIDTH * 0.5) - (dimensionLaserR.x * 0.5);
+	player.y = (WINDOW_HIGHT * 0.5) - (dimensionLaserR.y * 0.5);
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	ListItem<Bullet*>* item;
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		if(item->data->pendingToDelete == true) 
+			DeleteBody(item->data);
+	}
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	ListItem<Bullet*>* item;
+	// Create one bullet
+	if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+	{
+		AddBullet();
+
+		// Play Fx
+		for (item = bullets.start; item != NULL; item = item->next)
+		{
+			app->audio->PlayFx(item->data->channel, laserFx);
+		}
+	}
+	// Explosion bullets all direction
+	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_DOWN)
+	{
+		for (int i = 0; i < 360 / offsetAngle; i++)
+		{
+			AddBullet();
+
+			// Play Fx
+			for (item = bullets.start; item != NULL; item = item->next)
+			{
+				app->audio->PlayFx(item->data->channel, laserFx);
+			}
+		}
+	}
+	// Cerate one bullet in the direction of the mouse 
+	if (app->input->GetMouseButtonDown(SDL_BUTTON_LEFT)==KEY_DOWN)
+	{
+		iPoint posMouse;
+		app->input->GetMousePosition(posMouse.x, posMouse.y);
+		float angle = atan2(player.y - posMouse.y, player.x - posMouse.x) * 180 / PI;
+		AddBullet(angle + 180);
+
+		// Play Fx
+		for (item = bullets.start; item != NULL; item = item->next)
+		{
+			app->audio->PlayFx(item->data->channel, laserFx);
+		}
+	}
+
+	// Update entities positions
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		item->data->pos.x += speed * cos(item->data->angle * PI/180);
+		item->data->pos.y += speed * sin(item->data->angle * PI/180);
+	}
+
+	// TODO 5: Update distance and direction
+	// 0 = directly in front / 90 = directly to the right / 180 = directly behind / 270 = directly to the left.
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		app->audio->SetDistanceFx(item->data->channel, item->data->angle + 90, DistanceToListener(player, item->data->pos), WINDOW_WIDTH/2);
+	}
+
+	// TODO 7: Up/Down Music
+	if (app->input->GetKey(SDL_SCANCODE_KP_PLUS) == KEY_DOWN)
+		app->audio->ChangeMusicVolume(10);
+	if (app->input->GetKey(SDL_SCANCODE_KP_MINUS) == KEY_DOWN)
+		app->audio->ChangeMusicVolume(-10);
+
+	if (app->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN)
+		app->audio->ChangeFxVolume(10);
+	if (app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN)
+		app->audio->ChangeFxVolume(-10);
+
+	// Play Music
+	if (app->input->GetKey(SDL_SCANCODE_Z) == KEY_DOWN)
+	{
+		app->audio->PlayMusic("Assets/Audio/Music/galactic_empire.ogg");
+	}
+	if (app->input->GetKey(SDL_SCANCODE_X) == KEY_DOWN)
+	{
+		app->audio->PlayMusic("Assets/Audio/Music/imperial_march.ogg");
+	}
+
+	// Check if the bullet is off-camera
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		if (item->data->pos.x > WINDOW_WIDTH + MARGIN || item->data->pos.x < 0 - MARGIN
+			|| item->data->pos.y > WINDOW_HIGHT + MARGIN || item->data->pos.y < 0 - MARGIN)
+		{
+			item->data->pendingToDelete = true;
+		}
+	}
+
+	// TODO 6: If an entity has been released a channel too
+	if (app->audio->GetPendingToDelete() == true)
+	{
+		// TODO 6: If no channel is playing reassign the channels 
+		if (app->audio->RemoveChannel())
+		{
+			for (item = bullets.start; item != NULL; item = item->next)
+			{
+				item->data->channel = app->audio->SetChannel();
+			}
+		}
+	}
+	
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	// Draw background
+	app->render->DrawTexture(bg, (WINDOW_WIDTH * 0.5) - (dimensionBg.x * 0.5), (WINDOW_HIGHT * 0.5) - (dimensionBg.y * 0.5));
+
+	// Draw all bullets
+	ListItem<Bullet*>* item;
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		app->render->DrawTexture(item->data->laserTex, item->data->pos.x, item->data->pos.y, 0, 1, item->data->angle);
+	}
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+void Scene::AddBullet(float angle)
+{
+	// Create new entity
+	Bullet* b = new Bullet;
+	bullets.Add(b);
+
+	// Assign texture
+	if (bullets.Count() % 2 == 0)
+		b->laserTex = laserR;
+	else
+		b->laserTex = laserB;
+
+	// Assign position
+	b->pos.x = player.x;
+	b->pos.y = player.y;
+
+	// Assign direction
+	if (angle == -1)
+	{
+		if (bullets.Count() == 1)
+			b->angle = 0;
+		else
+			b->angle = bullets.end->prev->data->angle + offsetAngle;
+	}
+	else 
+		b->angle = angle;
+
+	// TODO 3: Assign new channel
+	b->channel = app->audio->SetChannel();
+}
+
+int Scene::DistanceToListener(iPoint player, fPoint channel)
+{
+	iPoint pos;
+	pos.x = player.x - channel.x;
+	pos.y = player.y - channel.y;
+
+	return sqrt(pow(pos.x,2)+pow(pos.y,2));
+}
+
+// Delete one bullet
+void Scene::DeleteBody(Bullet* body)
+{
+	ListItem<Bullet*>* item;
+
+	for (item = bullets.start; item != NULL; item = item->next)
+	{
+		if (item->data == body)
+		{
+			bullets.Del(item);
+			// TODO 6: Notify the audio manager that a channel can be released 
+			app->audio->DeleteChannel();
+			break;
+		}
+	}
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	app->tex->UnLoad(bg);
+	app->tex->UnLoad(laserR);
+	app->tex->UnLoad(laserB);
+
+	bullets.Clear();
+
+	return true;
+}
---
+++ Solution/Source/Scene.h
@@ -0,0 +1,69 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include "Module.h"
+
+struct SDL_Texture;
+
+class Bullet
+{
+public:
+	fPoint pos;
+	SDL_Texture* laserTex;
+	int angle;
+	int channel;
+	bool pendingToDelete = false;
+};
+
+class Scene : public Module
+{
+public:
+
+	Scene();
+
+	// Destructor
+	virtual ~Scene();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	void AddBullet(float angle = -1);
+	int DistanceToListener(iPoint player, fPoint channel);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Delete entity
+	void DeleteBody(Bullet* body);
+
+private:
+	float speed = 0.1f;
+	int offsetAngle = 10;
+
+	SDL_Texture* bg;
+	SDL_Texture* laserR;
+	SDL_Texture* laserB;
+
+	iPoint dimensionBg;
+	iPoint dimensionLaserR;
+	iPoint dimensionLaserB;
+	iPoint player;
+
+	uint laserFx;
+
+	List<Bullet*> bullets;
+};
+
+#endif // __SCENE_H__
